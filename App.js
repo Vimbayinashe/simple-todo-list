b6e2d70a5b1ae845ea92058ed68bebfcd587ac8f
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-07-16 11:50:14 +0200

    Add ScrollView to list output

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 import Flexes from './components/Flexes';
 
 export default function App() {
@@ -31,13 +31,13 @@ export default function App() {
                 <Button title="ADD" onPress={addGoalHandler} />
             </View>
 
-            <View>
+            <ScrollView >
                 {courseGoals.map((goal)=> (
                     <View style={styles.listItem}key={goal}>
                         <Text>{goal}</Text>
                     </View>
                 ))}
-            </View>
+            </ScrollView>
 
         </View>
     );
